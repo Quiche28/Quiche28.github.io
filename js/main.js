5d81822f046c7b3fd6f2a9889aec51bb2b19e0db
Quiche28 <salgadomarines1@gmail.com>
2023-05-27 16:10:17 -0600

    Redirection

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
+if (location.protocol !== 'https:') {
+    location.replace(`https://${location.hostname}${location.pathname}${location.search}`);
+}
+
 window.addEventListener("scroll", function(){var header = document.querySelector("header"); header.classList.toggle("sticky", window.scrollY > 0);})
